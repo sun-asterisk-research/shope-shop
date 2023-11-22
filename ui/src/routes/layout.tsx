c55f92fdb288a7c272b788d4994090593c8e5f37
Nguyen Huu Kim <13513658+kimyvgy@users.noreply.github.com>
2023-11-22 14:45:31 +0700

    refactor: Don't cache route data except the product list pages

--- ui/src/routes/layout.tsx
+++ ui/src/routes/layout.tsx
@@ -5,15 +5,17 @@ import HeaderSection from "~/components/header-section/header-section";
 import type { AuthData } from "~/contexts/auth";
 import { AuthContext } from "~/contexts/auth";
 
-export const onGet: RequestHandler = async ({ cacheControl }) => {
-  // Control caching for this request for best performance and to reduce hosting costs:
-  // https://qwik.builder.io/docs/caching/
-  cacheControl({
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 5,
-  });
+export const onGet: RequestHandler = async ({ cacheControl, url }) => {
+  if (url.pathname === "/men-clothes/" || url.pathname === "/women-clothes/") {
+    // Control caching for this request for best performance and to reduce hosting costs:
+    // https://qwik.builder.io/docs/caching/
+    cacheControl({
+      // Always serve a cached response by default, up to a week stale
+      staleWhileRevalidate: 60 * 60 * 24 * 7,
+      // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+      maxAge: 5,
+    });
+  }
 };
 
 export const useServerTimeLoader = routeLoader$(() => {
